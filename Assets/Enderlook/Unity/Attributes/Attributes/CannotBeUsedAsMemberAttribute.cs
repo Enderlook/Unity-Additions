5ff3ac0003e3af85e2d246ca3556fb42d7c186f1
Enderlook <enderlook@gmail.com>
2020-02-11 12:34:08 -0300

    Add CannotBeUsedAsMemberAttribute

---
+++ Assets/Enderlook/Unity/Attributes/Attributes/CannotBeUsedAsMemberAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Enderlook.Unity.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class CannotBeUsedAsMemberAttribute : Attribute { }
+}
---
+++ Assets/Enderlook/Unity/Attributes/Attributes/Post Compiling Testing/CannotBeUsedAsMemberTesting.cs
@@ -0,0 +1,34 @@
+﻿using Enderlook.Unity.Attributes.AttributeUsage;
+using Enderlook.Unity.Attributes.AttributeUsage.PostCompiling.Attributes;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Attributes
+{
+    internal static class CannotBeUsedAsMemberTesting
+    {
+        private static HashSet<Type> types = new HashSet<Type>();
+
+        [ExecuteOnEachTypeWhenScriptsReloads]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by PostCompilingAssembliesHelper")]
+        private static void GetTypes(Type type)
+        {
+            if (type.IsDefined(typeof(CannotBeUsedAsMemberAttribute), false))
+                types.Add(type);
+        }
+
+        [ExecuteOnEachFieldOfEachTypeWhenScriptsReloads(AttributeUsage.FieldSerialization.EitherSerializableOrNotByUnity, 1)]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by PostCompilingAssembliesHelper")]
+        private static void GetFields(FieldInfo fieldInfo)
+        {
+            if (fieldInfo.CheckIfShouldBeIgnored(typeof(ShowIfAttribute)))
+                return;
+            if (types.Contains(fieldInfo.FieldType)) // We do this instead of checking if IsDefined because that method is quite slow (not tested)
+                Debug.LogException(new ArgumentException($"{fieldInfo.DeclaringType} has a field {fieldInfo.Name} of type {fieldInfo.FieldType} which can not be used as member because it has defined the attribute {nameof(CannotBeUsedAsMemberAttribute)}."));
+        }
+    }
+}
