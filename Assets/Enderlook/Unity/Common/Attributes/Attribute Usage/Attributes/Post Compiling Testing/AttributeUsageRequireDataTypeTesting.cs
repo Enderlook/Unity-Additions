e05d289ae18457f83d845550a1a19f47f54b4dfe
Enderlook <enderlook@gmail.com>
2020-02-17 16:12:08 -0300

    Refactor DoNotCheckAttributeTesting, and AttributeUsageRequireDataTypeTesting

--- Assets/Enderlook/Unity/Common/Attributes/Attribute Usage/Attributes/Post Compiling Testing/AttributeUsageRequireDataTypeTesting.cs
+++ Assets/Enderlook/Unity/Common/Attributes/Attribute Usage/Attributes/Post Compiling Testing/AttributeUsageRequireDataTypeTesting.cs
@@ -21,32 +21,28 @@ namespace Enderlook.Unity.Attributes.AttributeUsage
             }
         }
 
+        private static void CheckSomething(IEnumerable<Attribute> attributes, HashSet<Type> toIgnore, Type type, AttributeTargets checkIf, string message)
+        {
+            foreach (Attribute attribute in attributes)
+            {
+                Type attributeType = attribute.GetType();
+                if (!toIgnore.Contains(attributeType)
+                    && checkers.TryGetValue(attributeType, out (AttributeTargets targets, Action<Type, string> checker) value)
+                    && (value.targets & checkIf) != 0
+                    ) // Check if has the proper flag
+                    value.checker(type, message);
+            }
+        }
+
+        private static void CheckSomething(MemberInfo memberInfo, Type type, string memberType, AttributeTargets checkIf) => CheckSomething(memberInfo.GetCustomAttributes(), new HashSet<Type>(memberInfo.GetAttributeTypesThatShouldBeIgnored()), type, checkIf, $"{memberType} {memberInfo.Name} in {memberInfo.DeclaringType.Name} class");
+
         [ExecuteOnEachTypeWhenScriptsReloads(ExecuteOnEachTypeWhenScriptsReloads.TypeFlags.IsNonEnum, 1)]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by PostCompilingAssembliesHelper.")]
         private static void CheckClasses(Type type)
         {
-            if (type.CheckIfShouldBeIgnored(typeof(AttributeUsageMethodAttribute)))
-                return;
-            foreach (Attribute attribute in type.GetCustomAttributes())
-            {
-                Type attributeType = attribute.GetType();
-                if (checkers.TryGetValue(attributeType, out (AttributeTargets targets, Action<Type, string> checker) value)
-                    && (value.targets & AttributeTargets.Class) != 0 // Check if has the proper flag
-                    && type.CheckIfShouldBeIgnored(attributeType))
-                    value.checker(type, $"Class {type.Name}");
-            }
-        }
-
-        private static void CheckSomething(MemberInfo memberInfo, Type type, string memberType, AttributeTargets checkIf)
-        {
-            foreach (Attribute attribute in memberInfo.GetCustomAttributes())
-            {
-                Type attributeType = attribute.GetType();
-                if (checkers.TryGetValue(attributeType, out (AttributeTargets targets, Action<Type, string> checker) value)
-                    && (value.targets & checkIf) != 0 // Check if has the proper flag
-                    && !memberInfo.CheckIfShouldBeIgnored(attributeType))
-                    value.checker(type, $"{memberType} {memberInfo.Name} in {memberInfo.DeclaringType.Name} class");
-            }
+            HashSet<Type> toIgnore = new HashSet<Type>(type.GetAttributeTypesThatShouldBeIgnored());
+            if (!toIgnore.Contains(typeof(AttributeUsageMethodAttribute)))
+                CheckSomething(type.GetCustomAttributes(), toIgnore, type, AttributeTargets.Class, $"Class {type.Name}");
         }
 
         [ExecuteOnEachFieldOfEachTypeWhenScriptsReloads(FieldSerialization.EitherSerializableOrNotByUnity, 1)]
--- Assets/Enderlook/Unity/Common/Attributes/Attribute Usage/Attributes/Post Compiling Testing/DoNotCheckAttributeTesting.cs
+++ Assets/Enderlook/Unity/Common/Attributes/Attribute Usage/Attributes/Post Compiling Testing/DoNotCheckAttributeTesting.cs
@@ -1,6 +1,7 @@
 ﻿using Enderlook.Unity.Attributes.AttributeUsage.PostCompiling.Attributes;
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -11,25 +12,22 @@ namespace Enderlook.Unity.Attributes.AttributeUsage
     // TODO: This should be internal
     public static class DoNotCheckAttributeTesting
     {
+        private static string MUST_INHERIT_FROM_ATTRIBUTE = "Attribute " + nameof(DoNotCheckAttribute) + " can only have types that inherit from " + nameof(Attribute) + " in the field " + nameof(DoNotCheckAttribute.ignoreTypes) + ". The type {0} is not subclass of " + nameof(Attribute) + ". Found in {1}.";
+
+        private static void CheckAttributes(DoNotCheckAttribute attribute, string foundIn)
+        {
+            foreach (Type type in attribute?.ignoreTypes ?? Enumerable.Empty<Type>())
+                if (!type.IsSubclassOf(typeof(Attribute)))
+                    Debug.LogException(new ArgumentException(string.Format(MUST_INHERIT_FROM_ATTRIBUTE, type, foundIn)));
+        }
+
         [ExecuteOnEachTypeWhenScriptsReloads(0)]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by PostCompilingAssembliesHelper.")]
-        private static void GetAttributeFromMember(Type type)
-        {
-            if (type.GetCustomAttribute<DoNotCheckAttribute>(true) is DoNotCheckAttribute attribute)
-                foreach (Type _type in attribute.ignoreTypes)
-                    if (!_type.IsSubclassOf(typeof(Attribute)))
-                        Debug.LogException(new ArgumentException($"Attribute {nameof(DoNotCheckAttribute)} can only have types that inherit from {nameof(Attribute)} in the field {nameof(DoNotCheckAttribute.ignoreTypes)}. The type {type} is not subclass of {nameof(Attribute)}. Found in type {type.Name}."));
-        }
+        private static void GetAttributeFromMember(Type type) => CheckAttributes(type.GetCustomAttribute<DoNotCheckAttribute>(), $"type {type.Name}");
 
         [ExecuteOnEachMemberOfEachTypeWhenScriptsReloads(0)]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by PostCompilingAssembliesHelper.")]
-        private static void GetAttributeFromMember(MemberInfo memberInfo)
-        {
-            if (memberInfo.GetCustomAttribute<DoNotCheckAttribute>(true) is DoNotCheckAttribute attribute)
-                foreach (Type type in attribute.ignoreTypes)
-                    if (!type.IsSubclassOf(typeof(Attribute)))
-                        Debug.LogException(new ArgumentException($"Attribute {nameof(DoNotCheckAttribute)} can only have types that inherit from {nameof(Attribute)} in the field {nameof(DoNotCheckAttribute.ignoreTypes)}. The type {type} is not subclass of {nameof(Attribute)}. Found in member {memberInfo.MemberType} {memberInfo.Name} in class {memberInfo.ReflectedType}."));
-        }
+        private static void GetAttributeFromMember(MemberInfo memberInfo) => CheckAttributes(memberInfo.GetCustomAttribute<DoNotCheckAttribute>(), $"member {memberInfo.MemberType} {memberInfo.Name} in class {memberInfo.ReflectedType}");
 
         /// <summary>
         /// Check if this <paramref name="memberInfo"/> should be ignored when checking if has <paramref name="typeThatMayBeIgnored"/>.
@@ -37,11 +35,7 @@ namespace Enderlook.Unity.Attributes.AttributeUsage
         /// <param name="memberInfo">Member to check.</param>
         /// <param name="typeThatMayBeIgnored">Type of attribute that it's being checked.</param>
         /// <returns>Whenever it should be ignored or not.</returns>
-        public static bool CheckIfShouldBeIgnored(this MemberInfo memberInfo, Type typeThatMayBeIgnored)
-        {
-            DoNotCheckAttribute attribute = memberInfo.GetCustomAttribute<DoNotCheckAttribute>(true);
-            return attribute?.ignoreTypes.Contains(typeThatMayBeIgnored) ?? false;
-        }
+        public static bool CheckIfShouldBeIgnored(this MemberInfo memberInfo, Type typeThatMayBeIgnored) => memberInfo.GetAttributeTypesThatShouldBeIgnored().Contains(typeThatMayBeIgnored);
 
         /// <summary>
         /// Check if this <paramref name="type"/> should be ignored when checking if has <paramref name="typeThatMayBeIgnored"/>.
@@ -49,10 +43,28 @@ namespace Enderlook.Unity.Attributes.AttributeUsage
         /// <param name="type">Type to check.</param>
         /// <param name="typeThatMayBeIgnored">Type of attribute that it's being checked.</param>
         /// <returns>Whenever it should be ignored or not.</returns>
-        public static bool CheckIfShouldBeIgnored(this Type type, Type typeThatMayBeIgnored)
+        public static bool CheckIfShouldBeIgnored(this Type type, Type typeThatMayBeIgnored) => type.GetAttributeTypesThatShouldBeIgnored().Contains(typeThatMayBeIgnored);
+
+        /// <summary>
+        /// Get attribute types that should be ignored.
+        /// </summary>
+        /// <param name="memberInfo">Member to check.</param>
+        /// <returns>Attribute types that should be ignored.</returns>
+        public static IEnumerable<Type> GetAttributeTypesThatShouldBeIgnored(this MemberInfo memberInfo)
         {
-            DoNotCheckAttribute attribute = type.GetCustomAttribute<DoNotCheckAttribute>(true);
-            return attribute?.ignoreTypes.Contains(typeThatMayBeIgnored) ?? false;
+            DoNotCheckAttribute attribute = memberInfo.GetCustomAttribute<DoNotCheckAttribute>();
+            return attribute?.ignoreTypes ?? Enumerable.Empty<Type>();
+        }
+
+        /// <summary>
+        /// Get attribute types that should be ignored.
+        /// </summary>
+        /// <param name="type">Type to check.</param>
+        /// <returns>Attribute types that should be ignored.</returns>
+        public static IEnumerable<Type> GetAttributeTypesThatShouldBeIgnored(this Type type)
+        {
+            DoNotCheckAttribute attribute = type.GetCustomAttribute<DoNotCheckAttribute>();
+            return attribute?.ignoreTypes ?? Enumerable.Empty<Type>();
         }
     }
 }
