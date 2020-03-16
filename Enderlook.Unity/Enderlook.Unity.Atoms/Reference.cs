0c4f69728442f1de4a1ad232368496bd9cc91867
Enderlook <enderlook@gmail.com>
2020-03-15 23:03:38 -0300

    Add references in Atoms

---
+++ Enderlook.Unity/Enderlook.Unity.Atoms/Reference.cs
@@ -0,0 +1,18 @@
+﻿using Enderlook.Unity.Attributes;
+using UnityEngine;
+
+namespace Enderlook.Unity.Atoms
+{
+    public abstract class Reference
+    {
+        public enum ReferenceMode
+        {
+            Value,
+            ScriptableObject,
+            Other,
+        }
+
+        [field: SerializeField, IsProperty, Tooltip("Mode in which values are used.")]
+        public ReferenceMode Mode { get; private set; }
+    }
+}
