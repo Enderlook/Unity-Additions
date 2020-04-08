049c7e71a36d004561cb9445ae92e334ea61404c
Enderlook <enderlook@gmail.com>
2020-04-08 08:10:06 -0300

    Add DontDestroyOnLoad to Enderlook.Unity.Components

---
+++ Enderlook.Unity/Enderlook.Unity.Components/DontDestroyOnLoad.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Enderlook.Unity.Components
+{
+    [AddComponentMenu("Enderlook/Don't Destroy On Load")]
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake() => DontDestroyOnLoad(gameObject);
+    }
+}
