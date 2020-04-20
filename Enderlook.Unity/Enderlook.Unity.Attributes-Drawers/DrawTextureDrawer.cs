fcf213ed6479b46cc03cc4ac7683c8c98fac83e8
Enderlook <enderlook@gmail.com>
2020-04-19 23:02:37 -0300

    Add DrawTextureAttribute

---
+++ Enderlook.Unity/Enderlook.Unity.Attributes-Drawers/DrawTextureDrawer.cs
@@ -0,0 +1,134 @@
+﻿using Enderlook.Utils.Exceptions;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Attributes
+{
+    //[CustomPropertyDrawer(typeof(Sprite))]
+    [CustomPropertyDrawer(typeof(DrawTextureAttribute))]
+    internal class DrawTextureDrawer : SmartPropertyDrawer
+    {
+        private float additionalHeight;
+        private Texture texture;
+
+        protected override void OnGUISmart(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SetTexture(property);
+
+            Rect mainPosition = position;
+            Rect texturePosition;
+
+            if (attribute is DrawTextureAttribute drawTextureAttribute)
+            {
+                float height = drawTextureAttribute.height;
+                if (height == -1)
+                    height = position.height;
+
+                float width = drawTextureAttribute.width;
+                if (width == -1)
+                    width = height;
+
+                void SetTexturePositionInNewLine()
+                {
+                    // Not exactly sure why half of height. I think it's because additionalHeight is computed twice due GetPropertyHeight. Not sure...
+                    additionalHeight = height / 2;
+
+                    float x = position.x;
+                    if (drawTextureAttribute.centered)
+                        x += position.width / 2;
+
+                    // We must deduct the additional height that is added by GetPropertyHeight
+                    texturePosition = new Rect(x, position.y + position.height - additionalHeight, width, height);
+                }
+
+                void SetTexturePositionInSameLine()
+                {
+                    texturePosition = new Rect(position.x + mainPosition.width, position.y, height, width);
+                    additionalHeight = 0;
+                }
+
+                switch (drawTextureAttribute.hideMode)
+                {
+                    case DrawTextureAttribute.Hide.All:
+                        additionalHeight = 0;
+                        texturePosition = new Rect(position.x, position.y, height, width);
+                        break;
+                    case DrawTextureAttribute.Hide.None:
+                        if (drawTextureAttribute.drawOnSameLine)
+                            DrawDefault(height, width);
+                        else
+                        {
+                            SetTexturePositionInNewLine();
+                            EditorGUI.PropertyField(mainPosition, property, label, true);
+                        }
+                        break;
+                    case DrawTextureAttribute.Hide.Label:
+                        if (drawTextureAttribute.drawOnSameLine)
+                        {
+                            mainPosition = new Rect(position.x, position.y, position.width - Mathf.Max(GUI.skin.label.CalcSize(label).x, height), position.height);
+                            SetTexturePositionInSameLine();
+                        }
+                        else
+                            SetTexturePositionInNewLine();
+                        EditorGUI.PropertyField(mainPosition, property, GUIContent.none, true);
+                        break;
+                    case DrawTextureAttribute.Hide.Field:
+                        if (drawTextureAttribute.drawOnSameLine)
+                        {
+                            mainPosition = new Rect(position.x, position.y, Mathf.Min(GUI.skin.label.CalcSize(label).x, height), position.height);
+                            SetTexturePositionInSameLine();
+                        }
+                        else
+                            SetTexturePositionInNewLine();
+                        EditorGUI.LabelField(mainPosition, label);
+                        break;
+                    default:
+                        throw new ImpossibleStateException();
+                }
+            }
+            else
+                DrawDefault(position.height, position.width);
+
+            // If we have an sprite to show and we are in a repaint event to show it
+            if (Event.current.type != EventType.Repaint && property.objectReferenceValue != null)
+                return;
+
+            if (property.objectReferenceValue is Sprite sprite)
+            {
+                GUIContent content = new GUIContent
+                {
+                    tooltip = label.text + "\n" + label.tooltip,
+                    image = sprite.texture
+                };
+                EditorGUI.LabelField(texturePosition, content);
+            }
+            else if (property.objectReferenceValue != null)
+            {
+                EditorGUI.HelpBox(texturePosition, $"Doesn't have an object of type {typeof(Sprite)}.", MessageType.Error);
+                Debug.LogError($"Property {property.displayName} from {property.propertyPath} does't have an object of type {typeof(Sprite)}.");
+            }
+
+            void DrawDefault(float h, float w)
+            {
+                mainPosition = new Rect(position.x, position.y, position.width - h, position.height);
+                texturePosition = new Rect(position.x + mainPosition.width, position.y, h, w);
+                EditorGUI.PropertyField(mainPosition, property, label, true);
+            }
+        }
+
+        private void SetTexture(SerializedProperty property)
+        {
+            if (property.objectReferenceValue is Texture2D texture2D)
+                texture = texture2D;
+            else if (property.objectReferenceValue is Sprite sprite)
+                texture = sprite.texture;
+            else
+                texture = null;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+            => base.GetPropertyHeight(property, label) + (texture == null ? 0 : additionalHeight);
+    }
+}
---
+++ Enderlook.Unity/Enderlook.Unity.Attributes/DrawTextureAttribute.cs
@@ -0,0 +1,130 @@
+﻿using Enderlook.Unity.Attributes.AttributeUsage;
+
+using System;
+
+using UnityEngine;
+
+namespace Enderlook.Unity.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    [AttributeUsageRequireDataType(typeof(Sprite), typeof(Texture2D), includeEnumerableTypes = false)]
+    [AttributeUsageFieldMustBeSerializableByUnity]
+    public class DrawTextureAttribute : PropertyAttribute
+    {
+        [Flags]
+        public enum Hide
+        {
+            /// <summary>
+            /// The texture will be shown along with the label and field.
+            /// </summary>
+            None = 0,
+
+            /// <summary>
+            /// The label will be hide.
+            /// </summary>
+            Label = 1 << 0,
+
+            /// <summary>
+            /// The field will be hide.
+            /// </summary>
+            Field = 1 << 1,
+
+            /// <summary>
+            /// Both <see cref="Hide.Label"/> and <see cref="Hide.Field"/>.
+            /// </summary>
+            All = Label | Field,
+        }
+
+        /// <summary>
+        /// Whenever the texture will be drawn on the same line as the property or in a line bellow.<br/>
+        /// This is ignored if <see cref="hideMode"/> is <see cref="Hide.All"/>.
+        /// </summary>
+        public readonly bool drawOnSameLine;
+
+        /// <summary>
+        /// Whenever the textre will be centered.<br/>
+        /// This is ignored if <see cref="drawOnSameLine"/> is <see langword="true"/>.
+        /// </summary>
+        public readonly bool centered;
+
+        /// <summary>
+        /// Height of the <see cref="Rect"/> used to show the texture.<br/>
+        /// On -1, the height of the property is used.
+        /// </summary>
+        public readonly float height;
+
+        /// <summary>
+        /// Width of the <see cref="Rect"/> used to show the texture.<br/>
+        /// On -1, <see cref="height"/> is used.
+        /// </summary>
+        public readonly float width;
+
+        /// <summary>
+        /// Determines how it will be shown in the inspector.
+        /// </summary>
+        public readonly Hide hideMode;
+
+        /// <summary>
+        /// Draw the texture next to the field in the inspector.
+        /// </summary>
+        /// <param name="drawOnSameLine">Whenever the texture will be drawn on the same line as the property or in a line bellow.</param>
+        /// <param name="centered">Whenever the textre will be centered.<br/>
+        /// This is ignored if <paramref name="drawOnSameLine"/> is <see langword="true"/>.</param>
+        public DrawTextureAttribute(bool drawOnSameLine = true, bool centered = false) : this(Hide.None, -1, -1, drawOnSameLine, centered) { }
+
+        /// <summary>
+        /// Draw the texture below the field in the inspector.
+        /// </summary>
+        /// <param name="size">Size of the <see cref="Rect"/> used to show the texture.<br/>
+        /// On -1, the height of the property is used.</param>
+        /// <param name="drawOnSameLine">Whenever the texture will be drawn on the same line as the property or in a line bellow.</param>
+        /// <param name="centered">Whenever the textre will be centered.<br/>
+        /// This is ignored if <paramref name="drawOnSameLine"/> is <see langword="true"/>.</param>
+        public DrawTextureAttribute(float size, bool drawOnSameLine = false, bool centered = false) : this(Hide.None, size, size, drawOnSameLine, centered) { }
+
+        /// <summary>
+        /// Draw the texture below the field in the inspector.
+        /// </summary>
+        /// <param name="height">Height of the <see cref="Rect"/> used to show the texture.<br/>
+        /// On -1, the height of the property is used.</param>
+        /// <param name="width">Width of the <see cref="Rect"/> used to show the texture.<br/>
+        /// On -1, <paramref name="height"/> is used.</param>
+        /// <param name="drawOnSameLine">Whenever the texture will be drawn on the same line as the property or in a line bellow.</param>
+        /// <param name="centered">Whenever the textre will be centered.<br/>
+        /// This is ignored if <paramref name="drawOnSameLine"/> is <see langword="true"/>.</param>
+        public DrawTextureAttribute(float height, float width, bool drawOnSameLine = false, bool centered = false) : this(Hide.None, height, width, drawOnSameLine, centered) { }
+
+        /// <summary>
+        /// Draw the texture of the field in the inspector.
+        /// </summary>
+        /// <param name="hideMode">Configure parts of the property that won't be shown.</param>
+        /// <param name="size">Size of the <see cref="Rect"/> used to show the texture.<br/>
+        /// On -1, the height of the property is used.</param>
+        /// <param name="drawOnSameLine">Whenever the texture will be drawn on the same line as the property or in a line bellow.<br/>
+        /// This is ignored if <see cref="hideMode"/> is <see cref="Hide.All"/>.</param>
+        /// <param name="centered">Whenever the textre will be centered.<br/>
+        /// This is ignored if <paramref name="drawOnSameLine"/> is <see langword="true"/>.</param>
+        public DrawTextureAttribute(Hide hideMode, float size, bool drawOnSameLine = false, bool centered = false) : this(hideMode, size, size, drawOnSameLine, centered) { }
+
+        /// <summary>
+        /// Draw the texture of the field in the inspector.
+        /// </summary>
+        /// <param name="hideMode">Configure parts of the property that won't be shown.</param>
+        /// <param name="height">Height of the <see cref="Rect"/> used to show the texture.<br/>
+        /// On -1, the height of the property is used.</param>
+        /// <param name="width">Width of the <see cref="Rect"/> used to show the texture.<br/>
+        /// On -1, <paramref name="height"/> is used.</param>
+        /// <param name="drawOnSameLine">Whenever the texture will be drawn on the same line as the property or in a line bellow.<br/>
+        /// This is ignored if <see cref="hideMode"/> is <see cref="Hide.All"/>.</param>
+        /// <param name="centered">Whenever the textre will be centered.<br/>
+        /// This is ignored if <paramref name="drawOnSameLine"/> is <see langword="true"/>.</param>
+        public DrawTextureAttribute(Hide hideMode, float height, float width, bool drawOnSameLine = false, bool centered = false)
+        {
+            this.hideMode = hideMode;
+            this.height = height;
+            this.width = width;
+            this.drawOnSameLine = drawOnSameLine;
+            this.centered = centered;
+        }
+    }
+}
