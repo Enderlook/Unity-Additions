ec69a952ac666d15e1309423596c9731f920ac07
Enderlook <enderlook@gmail.com>
2020-09-12 10:21:07 -0300

    Add EditorGUIHelper.DrawHorizontalLine methods

--- Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Utils.UnityEditor/Editor/EditorGUIHelper.cs
+++ Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Utils.UnityEditor/Editor/EditorGUIHelper.cs
@@ -11,6 +11,16 @@ namespace Enderlook.Unity.Utils.UnityEditor
     /// </summary>
     public static class EditorGUIHelper
     {
+        private static readonly GUIStyle HORIZONTALA_LINE;
+
+        static EditorGUIHelper()
+        {
+            HORIZONTALA_LINE = new GUIStyle();
+            HORIZONTALA_LINE.normal.background = EditorGUIUtility.whiteTexture;
+            HORIZONTALA_LINE.margin = new RectOffset(0, 0, 4, 4);
+            HORIZONTALA_LINE.fixedHeight = 1;
+        }
+
         /// <summary>
         /// Add a header.
         /// </summary>
@@ -18,6 +28,25 @@ namespace Enderlook.Unity.Utils.UnityEditor
         public static void Header(string text) =>
             // https://www.reddit.com/r/Unity3D/comments/3b43pf/unity_editor_scripting_how_can_i_draw_a_header_in/
             EditorGUILayout.LabelField(text, EditorStyles.boldLabel);
+
+        /// <summary>
+        /// Draw an horizontal line of the specified color.
+        /// </summary>
+        /// <param name="color">Color of line.</param>
+        public static void DrawHorizontalLine(Color color)
+        {
+            // https://forum.unity.com/threads/horizontal-line-in-editor-window.520812/#post-3514941
+            Color old = GUI.color;
+            GUI.color = color;
+            GUILayout.Box(GUIContent.none, HORIZONTALA_LINE);
+            GUI.color = old;
+        }
+
+        /// <summary>
+        /// Draw a gray horizontal line.
+        /// </summary>
+        public static void DrawHorizontalLine()
+            => DrawHorizontalLine(Color.gray);
 
         /// <summary>
         /// Draw the grey script field of the target script of this custom editor.
