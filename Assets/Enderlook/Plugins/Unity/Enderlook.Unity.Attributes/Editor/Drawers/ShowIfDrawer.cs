e2b403d14596666a2325e244eda885a658f0ccc0
Enderlook <enderlook@gmail.com>
2020-10-08 11:38:02 -0300

    Add to ShowIfDrawer capability to check any object type

--- Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Attributes/Editor/Drawers/ShowIfDrawer.cs
+++ Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Attributes/Editor/Drawers/ShowIfDrawer.cs
@@ -9,28 +9,21 @@ namespace Enderlook.Unity.Attributes
     [CustomPropertyDrawer(typeof(ShowIfAttribute))]
     internal class ShowIfDrawer : SmartPropertyDrawer
     {
-        /// <summary>
-        /// If <see langword="true"/>, the property field is either disabled or hidden.
-        /// </summary>
-        private bool? active;
-
-        private ShowIfAttribute.ActionMode mode;
-
         protected override void OnGUISmart(Rect position, SerializedProperty property, GUIContent label)
         {
             ShowIfAttribute showIfAttribute = (ShowIfAttribute)attribute;
-            mode = showIfAttribute.mode;
+            ShowIfAttribute.ActionMode mode = showIfAttribute.mode;
 
-            if (IsActive)
+            if (mode == ShowIfAttribute.ActionMode.ShowHide)
             {
-                if (mode == ShowIfAttribute.ActionMode.ShowHide)
+                if (IsActive(showIfAttribute))
                     DrawField();
-                else if (mode == ShowIfAttribute.ActionMode.EnableDisable)
-                {
-                    EditorGUI.BeginDisabledGroup(active.Value);
-                    DrawField();
-                    EditorGUI.EndDisabledGroup();
-                }
+            }
+            else if (mode == ShowIfAttribute.ActionMode.EnableDisable)
+            {
+                EditorGUI.BeginDisabledGroup(IsActive(showIfAttribute));
+                DrawField();
+                EditorGUI.EndDisabledGroup();
             }
 
             void DrawField()
@@ -38,33 +31,28 @@ namespace Enderlook.Unity.Attributes
                 SerializedPropertyGUIHelper.GetGUIContent(helper, ref label);
                 EditorGUI.PropertyField(position, property, label, true);
             }
-
-            // Force re-checking on next draw
-            active = null;
         }
 
         protected override float GetPropertyHeightSmart(SerializedProperty property, GUIContent label)
-            => mode == ShowIfAttribute.ActionMode.EnableDisable || IsActive ?
-                EditorGUI.GetPropertyHeight(property, label, true)
-                : 0;
+        {
+            ShowIfAttribute showIfAttribute = (ShowIfAttribute)attribute;
+            if (IsActive(showIfAttribute) || showIfAttribute.mode == ShowIfAttribute.ActionMode.EnableDisable)
+                return EditorGUI.GetPropertyHeight(property, label, true);
+            return 0;
+        }
 
-        private bool IsActive {
-            get {
-                if (active.HasValue)
-                    return active.Value;
-
-                ShowIfAttribute showIfAttribute = (ShowIfAttribute)attribute;
-                mode = showIfAttribute.mode;
-
-                if (helper.TryGetParentTargetObjectOfProperty(out object parent))
-                {
-                    active = parent.GetValueFromFirstMember<bool>(showIfAttribute.nameOfConditional, true);
-                    active = active == showIfAttribute.goal;
-                    return active.Value;
-                }
-
-                return false;
+        private bool IsActive(ShowIfAttribute showIfAttribute)
+        {
+            if (helper.TryGetParentTargetObjectOfProperty(out object parent))
+            {
+                object value = parent.GetValueFromFirstMember(showIfAttribute.nameOfConditional, showIfAttribute.memberType);
+                bool active = value.Equals(showIfAttribute.compareTo);
+                if (!showIfAttribute.mustBeEqual)
+                    active = !active;
+                return active;
             }
+
+            return false;
         }
     }
 }
--- Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Attributes/Runtime/ShowIfAttribute.cs
+++ Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Attributes/Runtime/ShowIfAttribute.cs
@@ -29,24 +29,66 @@ namespace Enderlook.Unity.Attributes
         }
 
         public readonly string nameOfConditional;
-        public readonly bool goal;
         public readonly ActionMode mode;
+        public readonly bool mustBeEqual;
+        public readonly object compareTo;
+        public readonly Type memberType;
 
         /// <summary>
         /// Action to take depending of the condition.
         /// </summary>
         /// <param name="nameOfConditional">Action to take depending of the condition.</param>
-        /// <param name="goal">Required boolean state to show or enable the property.</param>
-        public ShowIfAttribute(string nameOfConditional, ActionMode mode = ActionMode.ShowHide, bool goal = true) : this(nameOfConditional, goal) => this.mode = mode;
+        /// <param name="memberType">Type of the <see cref="nameOfConditional"/>.</param>
+        /// <param name="compareTo">The conditional will be compated to this value.</param>
+        public ShowIfAttribute(string nameOfConditional, Type memberType, object compareTo, ActionMode mode = ActionMode.ShowHide)
+        {
+            this.nameOfConditional = nameOfConditional;
+            this.memberType = memberType;
+            this.mode = mode;
+            this.compareTo = compareTo;
+            mustBeEqual = true;
+        }
 
         /// <summary>
         /// Action to take depending of the condition.
         /// </summary>
-        /// <param name="goal">Required boolean state to show or enable the property.</param>
-        public ShowIfAttribute(string nameOfConditional, bool goal)
+        /// <param name="nameOfConditional">Action to take depending of the condition.</param>
+        /// <param name="memberType">Type of the <see cref="nameOfConditional"/>.</param>
+        /// <param name="compareTo">The conditional will be compated to this value.</param>
+        /// <param name="mustBeEqual">Whenever the conditional value must match or not <paramref name="compareTo"/>.</param>
+        /// <param name="mode"></param>
+        public ShowIfAttribute(string nameOfConditional, Type memberType, object compareTo, bool mustBeEqual = true, ActionMode mode = ActionMode.ShowHide)
         {
             this.nameOfConditional = nameOfConditional;
-            this.goal = goal;
+            this.mode = mode;
+            this.compareTo = compareTo;
+            this.mustBeEqual = mustBeEqual;
+            this.memberType = memberType;
+        }
+
+        /// <summary>
+        /// Action to take depending of the condition.
+        /// </summary>
+        /// <param name="goal">The conditional must be a boolean value equal to this.</param>
+        public ShowIfAttribute(string nameOfConditional, bool goal, ActionMode mode = ActionMode.ShowHide)
+        {
+            this.nameOfConditional = nameOfConditional;
+            this.mode = mode;
+            mustBeEqual = true;
+            compareTo = goal;
+            memberType = typeof(bool);
+        }
+
+        /// <summary>
+        /// Action to take if <paramref name="nameOfConditional"/> results in <see langword="true"/>.
+        /// </summary>
+        public ShowIfAttribute(string nameOfConditional, ActionMode mode = ActionMode.ShowHide)
+        {
+            this.nameOfConditional = nameOfConditional;
+            this.mode = mode;
+            mustBeEqual = true;
+            compareTo = true;
+            memberType = typeof(bool);
         }
     }
 }
