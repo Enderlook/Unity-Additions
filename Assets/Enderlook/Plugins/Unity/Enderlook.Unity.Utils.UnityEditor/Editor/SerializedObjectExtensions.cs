95be913757441e276af5d1d27c3cb0886b6bf414
Enderlook <enderlook@gmail.com>
2020-10-17 16:42:13 -0300

    Make PropertyFieldAutoSave, DrawFieldIfConfirmed and ToggleableField from SerializedObjectExtensions obsolete

--- Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Utils.UnityEditor/Editor/SerializedObjectExtensions.cs
+++ Assets/Enderlook/Plugins/Unity/Enderlook.Unity.Utils.UnityEditor/Editor/SerializedObjectExtensions.cs
@@ -20,6 +20,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="serializedProperty"><see cref="SerializedProperty"/> to show in the inspector.</param>
         /// <param name="includeChildren">If <see langword="true"/> the property including children is drawn.</param>
         /// <see url="https://docs.unity3d.com/ScriptReference/EditorGUILayout.PropertyField.html"/>
+        [Obsolete]
         public static void PropertyFieldAutoSave(this SerializedObject source, SerializedProperty serializedProperty, bool includeChildren = false)
         {
             EditorGUI.BeginChangeCheck();
@@ -34,6 +35,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="source">Instance where its executed this method.</param>
         /// <param name="serializedProperty">Name of the <see cref="SerializedProperty"/> to show in the inspector.</param>
         /// <param name="includeChildren">If <see langword="true"/> the property including children is drawn.</param>
+        [Obsolete]
         public static void PropertyFieldAutoSave(this SerializedObject source, string serializedProperty, bool includeChildren = false) => source.PropertyFieldAutoSave(source.FindProperty(serializedProperty), includeChildren);
 
         /// <summary>
@@ -66,6 +68,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="toShowField">Function which creates the field on Unity Inspector.</param>
         /// <param name="confirm">Whenever it should be drew or not.</param>
         /// <param name="reasonForUndo">Reason used when save undo operation.</param>
+        [Obsolete]
         private static void DrawFieldIfConfirmed<T>(this SerializedObject source, ref T field, Func<T> toShowField, bool confirm, string reasonForUndo)
         {
             if (confirm)
@@ -83,6 +86,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="serializedProperty"><see cref="SerializedProperty"/> to show in the inspector.</param>
         /// <param name="includeChildren">If <see langword="true"/> the property including children is drawn.</param>
         /// <param name="confirm">Whenever it should be drew or not.</param>
+        [Obsolete]
         private static void DrawFieldIfConfirmed(this SerializedObject source, SerializedProperty serializedProperty, bool includeChildren, bool confirm) => DrawIdentedIfConfirmed(() => source.PropertyFieldAutoSave(serializedProperty, includeChildren), confirm);
 
         /// <summary>
@@ -92,6 +96,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="serializedProperty">Name of the <see cref="SerializedProperty"/> to show in the inspector.</param>
         /// <param name="includeChildren">If <see langword="true"/> the property including children is drawn.</param>
         /// <param name="confirm">Whenever it should be drew or not.</param>
+        [Obsolete]
         private static void DrawFieldIfConfirmed(this SerializedObject source, string serializedProperty, bool includeChildren, bool confirm) => DrawIdentedIfConfirmed(() => source.PropertyFieldAutoSave(serializedProperty, includeChildren), confirm);
 
         /// <summary>
@@ -99,6 +104,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// </summary>
         /// <param name="action"><see cref="Action"/> to be performed indented.</param>
         /// <param name="confirm">Whenever it should be drew or not.</param>
+        [Obsolete]
         private static void DrawIdentedIfConfirmed(Action action, bool confirm)
         {
             if (confirm)
@@ -122,6 +128,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="reasonForUndo">Reason used when save undo operation.<br/>
         /// If <see langword="null"/>, <paramref name="confirmationLabel"/> will be used instead.<br/>
         /// To this it's appended " Checkbox." or " Change value." depending which action it was done.</param>
+        [Obsolete]
         public static void ToggleableField<T>(this SerializedObject source, ref T field, Func<T> toShowField, ref bool confirmationVariable, string confirmationLabel, string confirmationTooltip = "", string reasonForUndo = null) => source.ToggleableField(ref field, toShowField, ref confirmationVariable, new GUIContent(confirmationLabel, confirmationTooltip), reasonForUndo ?? confirmationLabel);
 
         /// <summary>
@@ -138,6 +145,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// To this it's appended " Checkbox." or " Change value." depending which action it was done.</param>
         /// <see url="https://answers.unity.com/questions/192895/hideshow-properties-dynamically-in-inspector.html"/>
         /// <see url="https://www.reddit.com/r/Unity3D/comments/45bjwc/tooltip_on_custom_inspectorproperties/"/>
+        [Obsolete]
         public static void ToggleableField<T>(this SerializedObject source, ref T field, Func<T> toShowfield, ref bool confirmationVariable, GUIContent confirmationContent, string reasonForUndo)
         {
             reasonForUndo = reasonForUndo ?? confirmationContent.text;
@@ -158,6 +166,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="reasonForUndo">Reason used when save undo operation. To this it's appended " Checkbox.".<br/>
         /// If <see langword="null"/>, <c><paramref name="confirmationContent"/>.text</c> will be used instead.</param>
         /// <param name="includeChildren">If <see langword="true"/> the property including children is drawn.</param>
+        [Obsolete]
         public static void ToggleableField(this SerializedObject source, SerializedProperty serializedProperty, ref bool confirmationVariable, GUIContent confirmationContent, string reasonForUndo, bool includeChildren = false)
         {
             reasonForUndo = reasonForUndo ?? confirmationContent.text;
@@ -178,6 +187,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// If <see langword="null"/>, <c><paramref name="confirmationContent"/>.text</c> will be used instead.</param>
         /// <param name="includeChildren">If <see langword="true"/> the property including children is drawn.</param>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0054:Use compound assignment", Justification = "Unity doesn't support it.")]
+        [Obsolete]
         public static void ToggleableField(this SerializedObject source, string serializedProperty, ref bool confirmationVariable, GUIContent confirmationContent, string reasonForUndo, bool includeChildren = false)
         {
             reasonForUndo = reasonForUndo ?? confirmationContent.text;
@@ -194,6 +204,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="serializedProperty"><see cref="SerializedProperty"/> to show in the inspector.</param>
         /// <param name="booleanSerializedProperty"><see cref="SerializedProperty"/> to show in the inspector as confirmation checkbox.</param>
         /// <param name="includeChildren">If <see langword="true"/> the <paramref name="serializedProperty"/> including children is drawn.</param>
+        [Obsolete]
         public static void ToggleableField(this SerializedObject source, SerializedProperty serializedProperty, SerializedProperty booleanSerializedProperty, bool includeChildren = false)
         {
             source.PropertyFieldAutoSave(booleanSerializedProperty);
@@ -207,6 +218,7 @@ namespace Enderlook.Unity.Utils.UnityEditor
         /// <param name="serializedProperty">Name of the <see cref="SerializedProperty"/> to show in the inspector.</param>
         /// <param name="booleanSerializedProperty">Name of the <see cref="SerializedProperty"/> to show in the inspector as confirmation checkbox.</param>
         /// <param name="includeChildren">If <see langword="true"/> the <paramref name="serializedProperty"/> including children is drawn.</param>
+        [Obsolete]
         public static void ToggleableField(this SerializedObject source, string serializedProperty, string booleanSerializedProperty, bool includeChildren = false) => source.ToggleableField(source.FindProperty(serializedProperty), source.FindProperty(booleanSerializedProperty), includeChildren);
 
         /// <summary>
